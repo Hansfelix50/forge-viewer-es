1607e4f693c9b50cfdd7a0a2516c2e6c25f28e96
Hans Felix <hansfelix50@gmail.com>
2020-01-30 18:13:47 -0500

    markup extension

--- js/extensions/markup-extension.js
+++ js/extensions/markup-extension.js
@@ -6,13 +6,36 @@ MarkupExtension.prototype = Object.create(Autodesk.Viewing.Extension.prototype);
 MarkupExtension.prototype.constructor = MarkupExtension;
 
 MarkupExtension.prototype.load = function () {
-    // Set background environment to "Infinity Pool"
-    // and make sure the environment background texture is visible
+    let viewer = this.viewer;
+
     this.viewer.setLightPreset(6);
     this.viewer.setEnvMapBackground(true);
 
     // Ensure the model is centered
     this.viewer.fitToView();
+
+    //Create a Custom toolbar for markup
+    this.toolBarMarkup = new Autodesk.Viewing.UI.ToolBar('toolBarMarkup', { collapsible: true, alignVertically: false });
+
+    var controlGroupDraw = new Autodesk.Viewing.UI.ControlGroup('controlGroupDraw');
+
+    var buttonSquare = new Autodesk.Viewing.UI.ComboButton('buttonSquare');
+    buttonSquare.setToolTip('Cuadrado');
+    buttonSquare.setIcon('adsk-icon-measure-area-new');
+    let toolBarMarkup = this.toolBarMarkup;
+    buttonSquare.onClick = function (e) {
+        let markupExtension = viewer.getExtension('Autodesk.Viewing.MarkupsCore');
+        markupExtension.leaveEditMode();
+        markupExtension.hide();
+        toolBarMarkup.setVisible(false);
+    }
+    controlGroupDraw.addControl(buttonSquare);
+
+    // Add Control Group to Toolbar
+    this.toolBarMarkup.addControl(controlGroupDraw);
+    this.toolBarMarkup.setVisible(false);
+
+    this.viewer.container.append(this.toolBarMarkup.container)
 
     return true;
 };
@@ -20,69 +43,70 @@ MarkupExtension.prototype.load = function () {
 MarkupExtension.prototype.onToolbarCreated = function (toolbar) {
 
 
-    var viewer = this.viewer;
+    // var viewer = this.viewer;
 
-    // Crear Toolbar vertical
-    var toolbar2 = new Autodesk.Viewing.UI.ToolBar('toolbar-markup', { collapsible: true, alignVertically: true });
+    // // Crear Toolbar vertical
+    // var toolbar2 = new Autodesk.Viewing.UI.ToolBar('toolbar-markup', { collapsible: true, alignVertically: true });
 
-    var ctrlGroup = new Autodesk.Viewing.UI.ControlGroup('Autodesk.Research.TtIf.Extension.Toolbar.ControlGroup');
-    ctrlGroup.addClass('toolbar-vertical-group');
+    // var ctrlGroup = new Autodesk.Viewing.UI.ControlGroup('Autodesk.Research.TtIf.Extension.Toolbar.ControlGroup');
+    // ctrlGroup.addClass('toolbar-vertical-group');
 
 
-    var button2 = new Autodesk.Viewing.UI.ComboButton('show-env-bg-button');
-    button2.addClass('show-env-bg-button');
-    button2.setToolTip('Create a markup');
-    button2.setIcon('adsk-icon-measure-area-new');
-    ctrlGroup.addControl(button2);
+    // var button2 = new Autodesk.Viewing.UI.ComboButton('show-env-bg-button');
+    // button2.addClass('show-env-bg-button');
+    // button2.setToolTip('Create a markup');
+    // button2.setIcon('adsk-icon-measure-area-new');
+    // ctrlGroup.addControl(button2);
 
-    toolbar2.addControl(ctrlGroup);
-    // viewer.addControl(ctrlGroup);
-    toolbar2.setVisible(true);
+    // toolbar2.addControl(ctrlGroup);
+    // // viewer.addControl(ctrlGroup);
+    // toolbar2.setVisible(true);
 
 
 
 
     //toolbar3
-    var toolbar3 = new Autodesk.Viewing.UI.ToolBar('toolbar-hide', { collapsible: true, alignVertically: false });
-    var button3 = new Autodesk.Viewing.UI.Button('show-env-bg-button');
-    button2.addControl(button3);
-    toolbar3.setVisible(true);
+    // var toolbar3 = new Autodesk.Viewing.UI.ToolBar('toolbar-hide', { collapsible: true, alignVertically: false });
+    // var button3 = new Autodesk.Viewing.UI.Button('show-env-bg-button');
+    // button2.addControl(button3);
+    // toolbar3.setVisible(true);
     // button2.container.append(toolbar3.container);
 
 
-//
-    this.viewer.container.append(toolbar2.container)
-    debugger;
+    //
+    // this.viewer.container.append(toolbar2.container)
+    // debugger;
 
-    // Button 1
-    var button1 = new Autodesk.Viewing.UI.Button('show-env-bg-button');
-    button1.onClick = function (e) {
-        // viewer.setEnvMapBackground(true);
+    // Button 1}/
+    // PAra aactivar modo texto supuestamente
+    // var modeText = new Autodesk.Markups.Core.EditModeText(MarkupsExtension);
+    // MarkupsExtension.changeEditMode(modeText);
 
-        let MarkupsExtension = viewer.getExtension('Autodesk.Viewing.MarkupsCore');
-        MarkupsExtension.enterEditMode();
 
-        var modeText = new Autodesk.Markups.Core.EditModeText(MarkupsExtension);
-        MarkupsExtension.changeEditMode(modeText);
+    // Añadir el boton de crear markup al toolbar
+    var buttonMarkup = new Autodesk.Viewing.UI.Button('buttonMarkup');
+    buttonMarkup.setToolTip('Crear un markup');
+    buttonMarkup.setIcon('adsk-icon-measure-area-new'); // TODO: mejorar botón, se podría usar css
 
+    let toolBarMarkup = this.toolBarMarkup;
+    buttonMarkup.onClick = function (e) {
+        let markupExtension = viewer.getExtension('Autodesk.Viewing.MarkupsCore');
+        markupExtension.enterEditMode();
+        toolBarMarkup.setVisible(true);
     };
-    button1.addClass('show-env-bg-button');
-    button1.setToolTip('Create a markup');
-    button1.setIcon('adsk-icon-measure-area-new');
 
+    // Control Group para el markup
+    this.controlGroupMarkup = new Autodesk.Viewing.UI.ControlGroup('controlGroupMarkup');
+    this.controlGroupMarkup.addControl(buttonMarkup);
 
-    // SubToolbar
-    this.subToolbar = new Autodesk.Viewing.UI.ControlGroup('my-custom-toolbar');
-    this.subToolbar.addControl(button1);
-
-    toolbar.addControl(this.subToolbar);
+    toolbar.addControl(this.controlGroupMarkup);
 
 };
 
 MarkupExtension.prototype.unload = function () {
-    if (this.subToolbar) {
-        this.viewer.toolbar.removeControl(this.subToolbar);
-        this.subToolbar = null;
+    if (this.controlGroupMarkup) {
+        this.viewer.toolbar.removeControl(this.controlGroupMarkup);
+        this.controlGroupMarkup = null;
     }
 };
 
